26da46d856c0d2e1802b03ff5e83ab76f8f37301
jv <jeff@jeffvier.com>
2013-01-24 15:21:05 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 puppet-ucspi-tcp
 ================
 
-install's DJB's ucspi-tcp
+* Install & manage ucspi-tcp (required for djbdns/tinydns)
+
+* Notes:<ul><li>Defaults to pulling a specific version directly from http://cr.yp.to/daemontools/.
+This isn't particularly ideal/best practice.
+You should fork this project and make it install a package from your locally hosted binary repo.
+</ul>
+
+##Requirements##
+
+* wget
+
+##Usage##
+
+###Basic
+```puppet
+class { 'ucspi::install': }
+```
+##License##
+
+ Copyright (C) 2013 Jeff Vier <jeff@jeffvier.com> (Author)<br />
+ License: Apache 2.0
---
+++ manifests/install.pp
@@ -0,0 +1,43 @@
+class ucspi-tcp::install (
+    $pkg_url = 'http://cr.yp.to/ucspi-tcp/ucspi-tcp-0.88.tar.gz'
+  ){
+
+  if $pkg_url =~ /^.*\/([^\/]*)$/ { $pkg_tarball_name = $1 }
+  if $pkg_tarball_name =~ /^(.*)\.tar\.gz$/ { $pkg_name = $1 }
+
+  ## Sure, we'd like an rpm.  But there isn't one in the main distro's repo.
+  ## If you have an rpm (or deb, I guess) of daemontools, just uncomment this
+  ## and comment out the manual installation below.
+
+  ## Simple package installation:
+  #  package{'ucspi-tcp':
+  #    ensure => installed,
+  #  }
+
+  ## Manual installation of daemontools
+
+  file {
+    "daemontools conf-cc":
+      mode    => 644,
+      owner   => root,
+      group   => root,
+      path    => "/usr/local/src/$pkg_name/conf-cc",
+      source  => "puppet:///modules/ucspi-tcp/conf-cc",
+      require => Exec['get daemontools'];
+  }
+
+  exec {
+    'get ucspi-tcp':
+      cwd     => '/usr/local/src',
+      command => "/usr/bin/wget -q ${pkg_url} && tar xpfz ${pkg_tarball_name} && rm /package/${pkg_tarball_name}",
+      creates => "/usr/local/src/${pkg_name}";
+
+    'install ucspi-tcp':
+      cwd     => "/usr/local/src/${pkg_name}",
+      command => "make && make setup check",
+      creates => '/usr/local/bin/tcpserver',
+      require => Exec['get ucspi-tcp'];
+  }
+
+  ## End manual installation
+}
